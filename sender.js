fa5ad8a43c8d39cca7bf107a5fb26ca7d22b5138
Mevlut <sevincmevlut@gmail.com>
2019-09-22 15:32:22 +0300

    hello world

--- sender.js
+++ sender.js
@@ -2,24 +2,25 @@
 
 var amqp = require('amqplib/callback_api');
 
-console.log('test');
-
 amqp.connect('amqp://localhost', function(error0, connection) {
   if (error0) {
     throw error0;
   }
   connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
     var queue = 'hello';
-    var msg = 'Hello world';
+    var msg = 'Hello World from Sender!';
 
     channel.assertQueue(queue, {
       durable: false
     });
-
     channel.sendToQueue(queue, Buffer.from(msg));
+
     console.log(' [x] Sent %s', msg);
   });
-
   setTimeout(function() {
     connection.close();
     process.exit(0);
