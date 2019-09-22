27ec490bbd7bf3ddb872ab820996d46b83c04c8c
Mevlut <sevincmevlut@gmail.com>
2019-09-22 16:11:57 +0300

    work queues

---
+++ 02-work-queues/new-task.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    var queue = 'task_queue';
+    var msg = process.argv.slice(2).join(' ') || 'Hello World!';
+
+    // Create Queue
+    channel.assertQueue(queue, {
+      durable: true
+    });
+
+    // Send message to queue
+    channel.sendToQueue(queue, Buffer.from(msg), {
+      persistent: true
+    });
+
+    console.log(' [x] Sent %s', msg);
+  });
+  setTimeout(function() {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
